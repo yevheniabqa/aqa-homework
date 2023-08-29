5c30a83345760d5211344a63f94afe06132f4d88
yevheniabqa <yevheniab1993@gmail.com>
2023-08-29 22:22:33 +0300

    first commit

---
+++ lesson1/homework.py
@@ -0,0 +1,36 @@
+# Salary
+john_salary = 1033.30
+marta_salary = 1066.40
+
+print(john_salary, marta_salary)
+
+# Age
+john_age = 34
+marta_age = 30
+
+print(john_age, marta_age)
+
+# Name
+john_name = 'John'
+marta_name = 'Marta'
+
+print(john_name, marta_name)
+
+# Gender
+john_gender = False
+marta_gender = True
+
+print(john_gender, marta_gender)
+
+# Friends
+john_fiends = ["Billy", "Jane", "Sam"]
+marta_friends = ["Pam", "Kitty", "Dan"]
+
+# Name list
+names = ["Patty", "Selma", "Marge", "Homer", "Bart", "Patty", "Selma"]
+print(names)
+
+set_names = set(names)
+print(set_names)
+
+
---
+++ lesson3/lesson3.py
@@ -0,0 +1,9 @@
+# ткрнарнийй оператор
+even_number = True if 5 % 2 ==0 else False
+print(even_number)
+
+age = 23
+
+is_can_buy_beer = True if age >= 21 else False
+if is_can_buy_beer:
+    print('I can buy beer')
